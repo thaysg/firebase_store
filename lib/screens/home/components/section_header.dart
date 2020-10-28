a0f6c2b472bef98c1eeab44263fa45cbefae1dba
thaysg <thay.a.garcia@gmail.com>
2020-10-28 17:45:17 -0300

    commit

--- lib/screens/home/components/section_header.dart
+++ lib/screens/home/components/section_header.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
+// ignore: use_key_in_widget_constructors
 class SectionHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
